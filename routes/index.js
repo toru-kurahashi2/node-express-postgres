ce6c780c7bae4ff4b606f5ce2296b36f94761f2d
mikamihey <166688422+mikamihey@users.noreply.github.com>
2024-06-20 07:23:19 +0000

    mada

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg')
-let todos = [];
+const pg = require('pg');
 
+const connection = new pg.Pool({
+  host: 'localhost',
+  user: 'root',
+  password: 'postgres',
+  database: 'todo_app',
+  port: 5432,
+  });
+  
 router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'ToDo App',
-    todos: todos,
+    todos: results.rows,
   });
 });
 
 router.post('/', function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/');
+  connection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      console.log(result.rows)
+      res.redirect('/');
+    }
+  );
 });
 
 module.exports = router;
